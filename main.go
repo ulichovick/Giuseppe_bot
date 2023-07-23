6a6e095f2b05d1e3a7f869b961cbe73fdb8a7d6f
ulichovick <ulichovick@gmail.com>
2023-07-22 23:01:53 -0500

    primera version, giuseppe se conecta a una api de dicci en ingles y nada mas...

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type Secret struct {
+	Secret string
+}
+
+func main() {
+	secret, errs := ioutil.ReadFile("./secrets.json")
+	if errs != nil {
+		log.Fatal("Error leyendo el archivo del secret!: ", errs)
+	}
+	var data Secret
+	errs = json.Unmarshal(secret, &data)
+	if errs != nil {
+		log.Fatal("Error extrayendo el secret: ", errs)
+	}
+
+	bot, err := tgbotapi.NewBotAPI(data.Secret)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Cuenta autorizada  %s \n", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	ctx := context.Background()
+	_, cancel := context.WithCancel(ctx)
+
+	updates := bot.GetUpdatesChan(u)
+
+	go getUpdates(updates, bot)
+
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+	cancel()
+
+}
+
+func getUpdates(updates tgbotapi.UpdatesChannel, bot *tgbotapi.BotAPI) {
+
+	for update := range updates {
+		if update.Message != nil {
+			log.Printf("%s escribio %s", update.Message.From.FirstName, update.Message.Text)
+			/* msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text) */
+			cmd := exec.Command("python", "test_py/test.py", "-w", string(update.Message.Text))
+			out, err := cmd.Output()
+			if err != nil {
+				log.Println(err)
+			}
+			text := "Definition: " + string(out)
+			//log.Println(out)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
+			msg.ReplyToMessageID = update.Message.MessageID
+			bot.Send(msg)
+		}
+	}
+}
